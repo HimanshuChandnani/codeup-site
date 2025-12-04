2495e453a5d08a6723e933ddb564e315ca1c81e7
Rykt <himanshu.chandnani2004@gmail.com>
2025-12-04 15:34:10 +0530

    ui fix

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -64,7 +64,7 @@ const AdminPage = () => {
         { eventKey: "userQueries", label: "User Queries", phone: "Queries", icon: <LucideFileQuestion /> },
         { eventKey: "academy", label: "Academy", phone: "Academy", icon: <GraduationCap /> },
         { eventKey: "notify", label: "Notify Users", phone: "Notify", icon: <Bell /> },
-        { eventKey: "timesheets", label: "Timesheets", phone: "Timesheets", icon: <Calendar /> },
+        { eventKey: "timesheets", label: "Timesheets", phone: "Sheets", icon: <Calendar /> },
     ];
 
     return (
